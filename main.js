8ef31a010986577682a26c588a297071f6eea91c
Aayush <ayushkokate1@gmail.com>
2023-10-29 02:04:22 +0530

    add crawl page

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hey I am Working");
+function main(){
+    if(process.argv.length < 3){
+        console.log("No website provided");
+        process.exit(1);
+    } 
+
+    if(process.argv.length > 3){
+        console.log("To many command line args");
+        process.exit(1);
+    }
+    
+    const baseURL = process.argv[2];
+    
+    console.log(`Starting crawl of ${baseURL}`);
+}
