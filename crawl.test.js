8bc17c0dd3cefc8e31a681effad2366fb0322c27
Aayush <ayushkokate1@gmail.com>
2023-10-29 02:03:15 +0530

    add htmlURL

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeUrl} = require("./crawl.js");
+const {normalizeUrl, getURLsFromHTML} = require("./crawl.js");
 const {test, expect} = require("@jest/globals");
 
 test("normalizeUrl strip protocol", () => {
@@ -21,6 +21,24 @@ test("normalizeUrl capital", () => {
     const input = "https://blog.boot.dev/path/";
     const actual = normalizeUrl(input);
     const expected = "blog.boot.dev/path";
+    // actual url matches the expected url
+    expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML", () => {
+    const inputHTMLBody =
+     `
+      <html>
+         <body>
+           <a href="https://blog.boot.dev/">
+              Boot.dev 
+           </a>
+        </body>
+     </html>
+    `
+    const inputBaseURL = "https://blog.boot.dev";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://blog.boot.dev/"];
     // actual url matches the expected url
     expect(actual).toEqual(expected);
 });
